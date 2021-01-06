de318e9f49725851b6f9b5759682a7f118b1fe4f
Waqas <wkaxali@gmail.com>
2021-01-06 19:40:54 +0500

    update rayan

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\signInSignUPcontroller;
+
+use App\Http\Controllers\AddMenucontroller;
+use App\Http\Controllers\CustomerViewcotroller;
+use App\Http\Controllers\OrderFlowController;
+
+use App\Http\Controllers\UpdateStocksController;
+
+
+use App\Http\Controllers\salesFlow;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,90 +23,115 @@ use App\Http\Controllers\signInSignUPcontroller;
 |
 */
 //Route::get('/getsignin1/{data}',[signInSignUPcontroller::class, 'signIn'] );
+
+
+
+Route::get('/fetchAllmenu',[AddMenucontroller::class, 'fetchAllMenu'] );
+Route::get('/fetchCategories',[AddMenucontroller::class, 'getCategories'] );
+Route::get('/fetchMenu/{CID}',[AddMenucontroller::class, 'fetchMenu'] );
+Route::get('/fetchCategoriesInOptions',[AddMenucontroller::class, 'getCategoriesForSelectMenu'] );
+
+
 Route::get('/getsignin/{data}',[signInSignUPcontroller::class, 'InsertAdmin'] );
+Route::get('/placeOrder/{data}',[OrderFlowController::class, 'OrderFlow'] );
+Route::get('/getOrderId/{oid}',[OrderFlowController::class, 'getOrderItem'] );
+Route::get('/getOrderId',[OrderFlowController::class, 'getOrderID']);
+Route::get('/viewCustomer',[OrderFlowController::class, 'viewCustomer']);
+Route::get('/transactionHistory',[OrderFlowController::class, 'transactionHistory']);
+Route::get('/companyLedger',[OrderFlowController::class, 'companyLedger']);
+Route::get('/viewStock',[OrderFlowController::class, 'viewStock']);
+Route::get('/spareParts',[OrderFlowController::class, 'spareParts']);
+
+// Test Functions
+Route::get('/getTransaction',[OrderFlowController::class, 'getTransaction']);
+Route::get('/scratchFunc',[OrderFlowController::class, 'scratchFunc']);
+
+
+Route::get('/ruautos/{data}',[UpdateStocksController::class, 'updateStockDetails'] );
+Route::get('/getAvailableProducts',[UpdateStocksController::class, 'getAllAvailableProducts'] );
+Route::get('/addSales/{data}',[salesFlow::class, 'SalesFlow'] );
+
+
 Route::get('/', function () {
     return view('signInSignUp');
+});
+Route::get('/investor', function () {
+    return view('investors');
+});
+Route::get('/invgl', function () {
+    return view('investorGeneralLedger');
 });
 
 Route::get('/db', function () {
     return view('dashboard');
 });
+Route::get('/AddProduct/{data}',[AddMenucontroller::class, 'fetchMenu'] );
 
-Route::get('/addMenu', function () {
+
+Route::get('/am', function () {
     return view('addMenu');
 });
-Route::get('/viewStock', function () {
-    return view('viewStock');
-});
 
-Route::get('/bookOrder', function () {
+Route::get('/bo', function () {
     return view('bookOrder');
 });
 
-Route::get('/companyLedger', function () {
+Route::get('/cl', function () {
     return view('companyLedger');
 });
-Route::get('/delivery', function () {
+Route::get('/d', function () {
     return view('delivery');
 });
-Route::get('/deliveryLetter', function () {
+Route::get('/dl', function () {
     return view('deliveryLetter');
 });
-Route::get('/increaseInPrice', function () {
+Route::get('/ip', function () {
     return view('increaseInPrice');
 });
-Route::get('/invoiceServices', function () {
+Route::get('/is', function () {
     return view('invoiceServices');
 });
-Route::get('/ledger', function () {
+Route::get('/l', function () {
     return view('ledger');
 });
-Route::get('/printSaleInvoice', function () {
+Route::get('/psi', function () {
     return view('printSaleInvoice');
 });
-Route::get('/Receiving', function () {
+Route::get('/rec', function () {
     return view('Receiving');
 });
-Route::get('/SalesAndComission', function () {
-    return view('SalesAndComission');
+Route::get('/sc', function () {
+    return view('salesandcomission');
 });
 Route::get('/stock', function () {
     return view('stock');
 });
-Route::get('/transactionHistory', function () {
+Route::get('/th', function () {
     return view('transactionHistory');
 });
-Route::get('/viewCustomers', function () {
+Route::get('/customer/{data}',[CustomerViewcotroller::class, 'customerinfo'] );
+Route::get('/vc', function () {
     return view('viewCustomers');
 });
-Route::get('/investorLedger', function () {
-    return view('investorLedger');
+Route::get('/sp', function () {
+    return view('viewSpareParts');
 });
-Route::get('/investors', function () {
-    return view('investors');
+Route::get('/vs', function () {
+    return view('viewStock');
 });
-
-Route::get('/investorGeneralLedger', function () {
-    return view('investorGeneralLedger');
+Route::get('/ajax', function () {
+    return view('ajax');
 });
-
-Route::get('/expense', function () {
+Route::get('/scratch', function () {
+    return view('scratch');
+});
+Route::get('/ex', function () {
     return view('expense');
 });
-
-Route::get('/attendance', function () {
-    return view('attendance');
+Route::get('/ct', function () {
+    return view('comissionAndTaxes');
+});
+Route::get('/s', function () {
+    return view('salesandc');
 });
 
-
-Route::get('/payRoll', function () {
-    return view('payRoll');
-});
-
-Route::get('/employerView', function () {
-    return view('employerView');
-});
-
-Route::get('/EmployeeTaskView', function () {
-    return view('EmployeeTaskView');
-});
