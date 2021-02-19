734163bfd9a6d151e7fd0b09ded5ea8b4bbab43c
Waqas <wkaxali@gmail.com>
2021-02-19 22:30:35 +0500

    efewfewfew

--- resources/views/quotationList.blade.php
+++ resources/views/quotationList.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
+    
     <link rel="stylesheet" href="{{asset('assets/css/sidebar.css')}}">
 
     <style>
