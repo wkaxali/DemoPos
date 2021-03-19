42382f0a0660705eee45f82f6608123e18d7fae5
Your Name <you@example.com>
2021-03-19 18:16:19 +0500

    coomiiit

--- resources/views/quotationlisthtml.blade.php
+++ resources/views/quotationlisthtml.blade.php
@@ -119,7 +119,7 @@
                     </div>
                 </div>
                 <div class="container-fluid">
-                    <div class="row" >
+                    <div class="row">
                         <div class="col-md-12">
                             <div class="table-responsive">
                         <table class="table table-bordered table-hover" id="quotationsTable">
