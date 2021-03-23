62077e25058dcee2f0a3fb7636df44b87b97b1e0
Your Name <you@example.com>
2021-03-23 19:35:44 +0500

    final from rayan

--- resources/views/investorLedger.blade.php
+++ resources/views/investorLedger.blade.php
@@ -231,7 +231,7 @@
                         <div class="row borderCustom">
                             <div class="col-md-5 offset-md-1">
                                 <label for="">Name</label>
-                                <select style="height: 25px !important; width: 158px !important; "
+                                <select 
                                     class="selectpicker form-control" data-live-search="true" id="investors"
                                     onchange="getInvestorDetails()">
 
