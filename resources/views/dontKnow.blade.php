516e161afd6ee5c81ee5cfbe11bdd456a4a4d10c
Zuha <Zuha@gmail.com>
2021-05-07 23:31:54 +0500

    ss

--- resources/views/dontKnow.blade.php
+++ resources/views/dontKnow.blade.php
@@ -287,7 +287,17 @@
             $('#myTable').DataTable();
         });
 
-       
+        function getAccounts(){
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    alert(this.responseText);
+                }
+            };
+            xhttp.open("GET", "./showAccounts/", true);
+            xhttp.send();
+            }
+ 
     </script>
 </body>
 
