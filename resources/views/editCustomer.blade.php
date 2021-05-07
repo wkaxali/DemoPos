2b4a41bca31eab9e3d3bac755aeccb48613042b0
uzair <you@example.com>
2021-05-08 00:38:08 +0500

    done

--- resources/views/editCustomer.blade.php
+++ resources/views/editCustomer.blade.php
@@ -163,6 +163,7 @@ crossorigin="anonymous">
 </script>
 <script type="text/javascript" charset="utf8"
 src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </script>
 <script>
 $(document).ready(function () {
