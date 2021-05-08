c58e7a53c7b877a57395a34d1912454dcef598e3
uzair <you@example.com>
2021-05-08 23:07:13 +0500

    done

--- resources/views/addEmployeePay.blade.php
+++ resources/views/addEmployeePay.blade.php
@@ -429,11 +429,11 @@ var row = table.insertRow(-1);
 var cell1 = row.insertCell(0);
 var cell2 = row.insertCell(1);
 var cell3 = row.insertCell(2);
-
+var cell4 = row.insertCell(3);
 cell1.innerHTML = allownceHeadID;
 cell2.innerHTML = amount;
 cell3.innerHTML = allownceHead;
-cell4.innerHTML = '<button calss="" onclick="deleteRow(this)">X</button>';
+cell4.innerHTML = '<button class="" onclick="deleteRow(this)">X</button>';
 
 
 
