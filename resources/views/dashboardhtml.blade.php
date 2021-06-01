682578e7dd974a60305cf07a093e67dcfbacfb27
uzair <you@example.com>
2021-06-01 23:00:39 +0500

    done

--- resources/views/dashboardhtml.blade.php
+++ resources/views/dashboardhtml.blade.php
@@ -1029,7 +1029,7 @@ role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
                                     </div>
                                 </div>
                                 <div class="col-md-4 market-update-gd">
-                                    <div class="market-update-block clr-block-3">
+                                    <div class="market-update-block clr-block-2">
                                         <div class="col-md-8 market-update-left">
                                             <h3>23</h3>
                                             <h4>New Messages</h4>
