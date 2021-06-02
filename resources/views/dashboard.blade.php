1790de37bf5a493ff7f6cc3acae0296682b7d25d
uzair <you@example.com>
2021-06-02 23:33:40 +0500

    one

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1255,15 +1255,8 @@ function drawChart() {
 
 
     </script>
-    <script>
-    <script> 
-        $(document).ready(function(){
-        setInterval(function(){
-            $("#allcards").load(window.location.href + " #allcards" );
-        }, 3000);
-        });
-</script>
-    </script>
+   
+    <
     <script>
      function getCustomerSales() {
             
