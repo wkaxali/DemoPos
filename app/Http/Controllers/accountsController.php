241fa8fa2bec01e52826c8f536342134be3731af
wkaxali <wkaxali@gmail.com>
2021-06-16 18:34:30 +0500

    setting the accounts

--- app/Http/Controllers/accountsController.php
+++ app/Http/Controllers/accountsController.php
@@ -23,6 +23,23 @@ class accountsController extends Controller
 
 
     }
+    public static function getAccountHeads(){
+        $data=DB:: select('select * from tblaccounts');
+    
+        $option='<option value=""></option>';
+    
+    
+        foreach ($data as $d){
+          //print $option;
+    
+            $option=$option.'
+            <option value= '.$d->AID.'>'.$d->AccountName.'   #    '.$d->AccountNumber.'</option>';
+          
+        }
+        return $option;
+
+
+    }
     public static function getAccountBalance($AID){
 
        
