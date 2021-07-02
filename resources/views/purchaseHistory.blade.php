b0978d2e326d9188c9cde8614e815d9ec7b4dbed
uzair <you@example.com>
2021-07-02 23:29:47 +0500

    done

--- resources/views/purchaseHistory.blade.php
+++ resources/views/purchaseHistory.blade.php
@@ -165,13 +165,17 @@
                                                 <button  class="btn  btn-info" data-live-search="true" id="dates" style="margin-top:2px;"
                                                         onclick="filterPurchaseDateData()">Search </button> </div>
                                             </div>
+                                            <div class="row my-2" >
+                                            <div class="col-md-4" >
                                             <label for="">Total Sale Amount:</label>
-                                            <h1 id="totalSaleAmount">0</h1>
+                                            <h1 id="totalSaleAmount">0</h1></div>
+                                            <div class="col-md-4" >
                                             <label for="">Total Amount Paid:</label>
-                                            <h1 id="remainingAmount">0</h1>
-                                            <label for="">Total Balance:</label>
-                                            <h1 id="invoiceBalance">0</h1>
-                                            
+                                            <h1 id="remainingAmount">0</h1></div>
+                                            <div class="col-md-4" >
+                                            <label for="">Total<br> Balance:</label>
+                                            <h1 id="invoiceBalance">0</h1></div>
+                                            </div>
                                         </div>
                                   </div>
                           
