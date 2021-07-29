3eecfe7f1380cc758b4cff51c45b76acb10238c0
Zuha <Zuha@gmail.com>
2021-07-29 21:14:05 +0500

    j

--- resources/views/editAutoModels.blade.php
+++ resources/views/editAutoModels.blade.php
@@ -8,12 +8,28 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
-    <title>Title Stock</title>
+    <title>Edit Auto Models</title>
 
 
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
+    <link rel="stylesheet" href="{{asset('assets/css/sidebar.css')}}">
 
     <style>
+
+.page-container.sidebar-collapsed-back .left-content {
+            transition: all 100ms linear;
+            -webkit-transition: all 0.3s ease;
+            -moz-transition: all 0.3s ease;
+            transition: all 0.3s ease;
+            float: right;
+            width: 84%;
+        }
         table th,
         td {
             border: 1px solid rgb(202, 202, 202);
@@ -39,9 +55,16 @@
 </head>
 
 <body onload="getAutoModel()">
+<div class="page-container">
 @include('mainNavbar')
+ 
+
+
+ <div class="clearfix"></div>
+</div>
     <header class="idi">
         <div class="container">
+     
             <div class="row">
                 <div class="col-md-12 text-center">
                     <h3 class="mt-3">Edit Auto Models</h3>
@@ -226,6 +249,33 @@ function editAutoModels() {
             
         
 </script>
+
+<script>
+        $(document).ready(function () {
+            $('#myTables').DataTable();
+        });
+        var toggle = true;
+
+        $(".sidebar-icon").click(function () {
+            if (toggle) {
+                $(".page-container").addClass("sidebar-collapsed").removeClass("sidebar-collapsed-back");
+                $("#menu span").css({
+                    "position": "absolute",
+
+                });
+            } else {
+                $(".page-container").removeClass("sidebar-collapsed").addClass("sidebar-collapsed-back");
+                setTimeout(function () {
+                    $("#menu span").css({
+                        "position": "relative",
+
+                    });
+                }, 400);
+            }
+            toggle = !toggle;
+        });
+
+    </script>
     
 </body>
 
