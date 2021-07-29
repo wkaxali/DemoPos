c6bb59e5cceadc9b1d41c6617f39150d1fee9ec5
uzair <you@example.com>
2021-07-29 21:33:32 +0500

    done

--- resources/views/addAllowance.blade.php
+++ resources/views/addAllowance.blade.php
@@ -196,6 +196,30 @@
     </script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js">
     </script>
+       <script>
+        
+        var toggle = true;
+
+        $(".sidebar-icon").click(function () {
+            if (toggle) {
+                $(".page-container").addClass("sidebar-collapsed").removeClass("sidebar-collapsed-back");
+                $("#menu span").css({
+                    "position": "absolute",
+
+                });
+            } else {
+                $(".page-container").removeClass("sidebar-collapsed").addClass("sidebar-collapsed-back");
+                setTimeout(function () {
+                    $("#menu span").css({
+                        "position": "relative",
+
+                    });
+                }, 400);
+            }
+            toggle = !toggle;
+        });
+
+    </script>
     <script>
         function addAllowance() {
 
