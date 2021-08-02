dbd8ea599292c24111ee9e2224db3c57534a3f5c
uzair <you@example.com>
2021-08-02 19:29:15 +0500

    dd

--- resources/views/editUsers.blade.php
+++ resources/views/editUsers.blade.php
@@ -210,6 +210,21 @@
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    
                     var data = this.responseText;
 
                     var table;
