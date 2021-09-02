4502c3e8edf65adb37d904138ee3f379481eb38b
Zuha <Zuha@gmail.com>
2021-09-02 23:02:42 +0500

    fghf

--- app/Http/Controllers/partyPaymentHistory.php
+++ app/Http/Controllers/partyPaymentHistory.php
@@ -15,7 +15,7 @@ class partyPaymentHistory extends Controller
     public static function filterPartyPaymentHistory($partyID){
         $data=0;
         if($partyID=="All"){
-            $data=DB:: select('select * from vw_transaction_flow where PaidTo IS NOT NULL and PaidTo <> 0 and TransactionCatogery <> "Amount Transfer  and');
+            $data=DB:: select('select * from vw_transaction_flow where PaidTo IS NOT NULL and PaidTo <> 0 and TransactionCatogery <> "Amount Transfer"');
         
         }
         else if($partyID!="All"){
