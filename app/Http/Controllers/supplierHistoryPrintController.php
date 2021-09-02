016d9bdbb26a5d3cdf37aa2604651f5b031a7c02
uzair <you@example.com>
2021-09-02 23:03:27 +0500

    dd

--- app/Http/Controllers/supplierHistoryPrintController.php
+++ app/Http/Controllers/supplierHistoryPrintController.php
@@ -256,69 +256,69 @@ class supplierHistoryPrintController extends Controller
         $table='<h1 align="center"> Vehicle Sale Details</h1>
         <br><br><br>
             
-    <table  align="center" cellpadding = "7" cellspacing = "0"  border="1" style="font-size:22.2px margin-left:49px;"><thead></thead>
-    <tbody>
-        <tr>
-        <th style="text-align:center;">Invoice Number</th>
-         
-        <th style="text-align:center;">DateStamp</th>
-     
-        <th style="text-align:center;">Total Amount</th>
-        <th style="text-align:center;">Net Total</th> 
-        <th style="text-align:center;">Amount Paid</th> 
-        <th style="text-align:center;">Sales Person</th> 
-        <th style="text-align:center;">Net Amount</th> 
-        <th style="text-align:center;">Sale Price</th> 
-        <th style="text-align:center;">Customer Name</th> 
-        <th style="text-align:center;">Father Name</th> 
-        <th style="text-align:center;">Contact</th> 
-        <th style="text-align:center;">Product Name</th> 
-     
-        <th style="text-align:center;">Engine Number</th> 
-        <th style="text-align:center;">Chasis Number</th> 
-        <th style="text-align:center;">color</th> 
-
-        </tr>
-     
-   
-      ';
-
-   
-   
-  foreach ($data as $d){
-      
-
-      $table=$table.'
-     
-      
-      <tr>
-      <td style="text-align:center;" height:80px;>'.$d->InvoiceNumber.'</td> 
-         
-        <td style="text-align:center;" height:80px;>'.$d->DateStamp.'</td>
-  
-        <td style="text-align:center;" height:80px;>'.$d->TotalAmount.'</td>
-      
-        <td height:80px;>'.$d->NetTotal.'</td>
-        <td height:80px;>'.$d->AmountPaid.'</td>
-      
-        <td height:80px;>'.$d->salesPerson.'</td>
+        <table  align="center" cellpadding = "7" cellspacing = "0"  border="1" style="font-size:22.2px margin-left:49px;"><thead></thead>
+        <tbody>
+            <tr>
+            <th style="text-align:center;">Invoice Number</th>
+            
+            <th style="text-align:center;">DateStamp</th>
         
-        <td height:80px;>'.$d->NetAmount.'</td>
-        <td height:80px;>'.$d->SalePrice.'</td>
-        <td height:80px;>'.$d->CustomerName.'</td>
-        <td height:80px;>'.$d->FatherName.'</td>
+            <th style="text-align:center;">Total Amount</th>
+            <th style="text-align:center;">Net Total</th> 
+            <th style="text-align:center;">Amount Paid</th> 
+            <th style="text-align:center;">Sales Person</th> 
+            <th style="text-align:center;">Net Amount</th> 
+            <th style="text-align:center;">Sale Price</th> 
+            <th style="text-align:center;">Customer Name</th> 
+            <th style="text-align:center;">Father Name</th> 
+            <th style="text-align:center;">Contact</th> 
+            <th style="text-align:center;">Product Name</th> 
+        
+            <th style="text-align:center;">Engine Number</th> 
+            <th style="text-align:center;">Chasis Number</th> 
+            <th style="text-align:center;">color</th> 
+
+            </tr>
+        
     
-        <td height:80px;>'.$d->Contect.'</td>
-       
-        <td height:80px;>'.$d->ProductName.'</td>
-      
-        <td height:80px;>'.$d->EngineNumber.'</td>
-        <td height:80px;>'.$d->ChasisNumber.'</td>
-        <td height:80px;>'.$d->color.'</td>
+        ';
+
     
-         
-      </tr>
-      
+    
+    foreach ($data as $d){
+        
+
+        $table=$table.'
+        
+        
+        <tr>
+        <td style="text-align:center;" height:80px;>'.$d->InvoiceNumber.'</td> 
+            
+            <td style="text-align:center;" height:80px;>'.$d->DateStamp.'</td>
+    
+            <td style="text-align:center;" height:80px;>'.$d->TotalAmount.'</td>
+        
+            <td height:80px;>'.$d->NetTotal.'</td>
+            <td height:80px;>'.$d->AmountPaid.'</td>
+        
+            <td height:80px;>'.$d->salesPerson.'</td>
+            
+            <td height:80px;>'.$d->NetAmount.'</td>
+            <td height:80px;>'.$d->SalePrice.'</td>
+            <td height:80px;>'.$d->CustomerName.'</td>
+            <td height:80px;>'.$d->FatherName.'</td>
+        
+            <td height:80px;>'.$d->Contect.'</td>
+        
+            <td height:80px;>'.$d->ProductName.'</td>
+        
+            <td height:80px;>'.$d->EngineNumber.'</td>
+            <td height:80px;>'.$d->ChasisNumber.'</td>
+            <td height:80px;>'.$d->color.'</td>
+        
+            
+        </tr>
+        
   
 
        ';}
@@ -329,52 +329,62 @@ class supplierHistoryPrintController extends Controller
            
      
     }
-    else if($cat=="All"){
-        $data=DB:: select('select * from vw_transaction_flow where Category="Supplier"');
-    
-    $table='<h1 align="center"> Supplier Transactions Details</h1>
-    <br><br><br>
-            
-    <table  align="center" cellpadding = "7" cellspacing = "0"  border="1" style="font-size:22.2px margin-left:49px;"><thead></thead>
-      <tbody>
-          <tr>
-          <th style="text-align:center;">Transaction ID</th>
-          <th style="text-align:center;">Party Name</th> 
-          <th style="text-align:center;">Account Name</th>
-       
-          <th style="text-align:center;">Total Purchase Amount</th>
+    else if($cat=="Booking Order"){
+        $in=DB:: table('vw_transaction_flow')
+        ->where('TransactionID','=',$id)
+        ->first()->InvoiceNo;
+        $data=DB:: select('select * from vw_purchaseorderdetails where InvoiceNumber='.$in);
       
-       
-          <th style="text-align:center;">Transaction Date</th> 
-              
-              
-          </tr>
-       
-     
-        ';
-
-     
-      $data=DB::select('select * from vw_transaction_flow where Category="Supplier"  and TransactionCatogery <> "Amount Transfer" and TransactionID ="'.$id.'"');
-    foreach ($data as $d){
+    $table='<h1 align="center"> Vehicle Sale Details</h1>
+    <br><br><br>
         
-
-        $table=$table.'
-       
-        
+    <table  align="center" cellpadding = "7" cellspacing = "0"  border="1" style="font-size:22.2px margin-left:49px;"><thead></thead>
+    <tbody>
         <tr>
-        <td style="text-align:center;" height:80px;>'.$d->TransactionID.'</td> 
-          <td style="text-align:center;" height:80px;>'.$d->PartyName.'</td>
-          <td style="text-align:center;" height:80px;>'.$d->AccountName.' ('.$d->AccountNumber.')</td>
+        <th style="text-align:center;">Invoice Number</th>
+        
+        <th style="text-align:center;">Product Serial</th>
     
-          <td style="text-align:center;" height:80px;>'.$d->Amount.'</td>
-          
+        <th style="text-align:center;">Product Name</th>
+        <th style="text-align:center;">Color</th> 
+        <th style="text-align:center;">Engine Number</th> 
+        <th style="text-align:center;">Chasis Number</th> 
+        <th style="text-align:center;">Amount Paid</th> 
+        <th style="text-align:center;">Balance</th> 
+        <th style="text-align:center;">DatePurchase</th> 
          
-        
-          
-          <td height:80px;>'.$d->DateStamp.'</td>
-        
+
         </tr>
+    
+
+    ';
+
+
+
+foreach ($data as $d){
+    
+
+    $table=$table.'
+    
+    
+    <tr>
+    <td style="text-align:center;" height:80px;>'.$d->InvoiceNumber.'</td> 
         
+        <td style="text-align:center;" height:80px;>'.$d->ProductSerial.'</td>
+
+        <td style="text-align:center;" height:80px;>'.$d->ProductName.'</td>
+    
+        <td height:80px;>'.$d->Color.'</td>
+        
+        <td height:80px;>'.$d->EngineNumber.'</td>
+        <td height:80px;>'.$d->ChasisNumber.'</td>
+        <td height:80px;>'.$d->AmountPaid.'</td>
+    
+        <td height:80px;>'.$d->Balance.'</td>
+        <td height:80px;>'.$d->DatePurchase.'</td>
+        
+        
+    </tr>
     
 
          ';}
