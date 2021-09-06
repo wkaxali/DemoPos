dc6232f36962c79a3c2068b3a5ac58c21d234329
Zuha <Zuha@gmail.com>
2021-09-06 16:35:40 +0500

    edfvf

--- app/Http/Controllers/partyPaymentHistory.php
+++ app/Http/Controllers/partyPaymentHistory.php
@@ -8,17 +8,17 @@ use DB;
 class partyPaymentHistory extends Controller
 {
     public static function getPartyPaymentHistory(){
-        $data=DB:: select('select * from vw_transaction_flow where (Category = "Supplier" or Category = "Investor") and TransactionCatogery <> "Amount Transfer"');
+        $data=DB:: select('select * from vw_all_transactions where (Category = "Supplier" or Category = "Investor") and TransactionCatogery <> "Amount Transfer"');
         return $data;
     }
     public static function filterPartyPaymentHistory($partyID){
         $data=0;
         if($partyID=="All"){
-            $data=DB:: select('select * from vw_transaction_flow where (Category = "Supplier" or Category = "Investor") and TransactionCatogery <> "Amount Transfer"');
+            $data=DB:: select('select * from vw_all_transactions where (Category = "Supplier" or Category = "Investor") and TransactionCatogery <> "Amount Transfer"');
         
         }
         else if($partyID!="All"){
-            $data=DB:: select('select * from vw_transaction_flow where TransactionCatogery <> "Amount Transfer" and PaidTo ='.$partyID);
+            $data=DB:: select('select * from vw_all_transactions where TransactionCatogery <> "Amount Transfer" and PaidTo ='.$partyID);
         
         }
 
@@ -28,11 +28,11 @@ class partyPaymentHistory extends Controller
     public static function filterPartyPaymentDateData($date1,$date2,$partyID){
         $data=0;
         if($partyID=="All"){
-            $data=DB:: select('select * from vw_transaction_flow where (Category = "Supplier" or Category = "Investor")  and TransactionCatogery <> "Amount Transfer" and DateStamp between "'.$date1.'"and"'.$date2.'"');
+            $data=DB:: select('select * from vw_all_transactions where (Category = "Supplier" or Category = "Investor")  and TransactionCatogery <> "Amount Transfer" and DateStamp between "'.$date1.'"and"'.$date2.'"');
         
         }
         else if($partyID!="All"){
-            $data=DB:: select('select * from vw_transaction_flow where PaidTo='.$partyID.' and TransactionCatogery <> "Amount Transfer" and DateStamp between "'.$date1 .'" and "'.$date2.'"');
+            $data=DB:: select('select * from vw_all_transactions where PaidTo='.$partyID.' and TransactionCatogery <> "Amount Transfer" and DateStamp between "'.$date1 .'" and "'.$date2.'"');
         
         }
         return $data;
