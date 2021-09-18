55aed76635564232fd218b447ff61ee4cbc5cf9b
uzair <you@example.com>
2021-09-18 12:35:54 +0500

    done

--- app/Http/Controllers/OrderFlowController.php
+++ app/Http/Controllers/OrderFlowController.php
@@ -300,7 +300,7 @@ class OrderFlowController extends Controller
       }
 
       function getboHistory(){
-        $data=DB:: select('select * from tblpurchaseorder where BillStatus = "Pending"');
+        $data=DB:: select('select * from tblpurchaseoorderdetaile where DilevedStatus = "Pending"');
         return $data;
       }
 
