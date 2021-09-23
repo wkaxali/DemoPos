c5a3605ef94df8584e3df1105e888dd025c565fe
Zuha <Zuha@gmail.com>
2021-09-23 21:19:15 +0500

    dfs

--- app/Http/Controllers/receivedAutosReturn.php
+++ app/Http/Controllers/receivedAutosReturn.php
@@ -25,19 +25,24 @@ class receivedAutosReturn extends Controller
         $currentBalance=floatval($oldBalance)-floatval($oldPrice);
         LedgerPartiesController::UpdatePartiesBalance($FJW,$currentBalance);
 
-        TransactionFlow::addTransaction("","Credit",'Vehicle Return',$oldPrice,$dateNow,
-        NULL,$oldBalance,$currentBalance,NULL,null,1,NULL,NULL,2,NULL,NULL,Null);
-        
         DB::table('instock')
         ->where('ProductSerial', $PID)
         ->update(['Remarks'=>"Returned on ".$dateNow,
         'Status'=>'Returned'
         ]);
 
+        $InvoiceNo = DB::table('tblpurchaseoorderdetaile')
+        ->where('ProductSerial', $PID)
+        ->first()->InvoiceNumber;
+
         DB::table('tblpurchaseoorderdetaile')
         ->where('ProductSerial', $PID)
-        ->update(['DilevedStatus'=>"Returned"
+        ->update([
+            'DilevedStatus'=>"Returned"
         ]);
+
+        TransactionFlow::addTransaction($InvoiceNo,"Credit",'Vehicle Return',$oldPrice,$dateNow,
+        NULL,$oldBalance,$currentBalance,NULL,null,1,NULL,NULL,2,NULL,NULL,Null);
     
     
         return $PID;
