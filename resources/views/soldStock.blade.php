8bcce0cb2e010dbc81bc1f74a38029ff5700c84b
uzair <you@example.com>
2021-09-24 21:59:52 +0500

    done

--- resources/views/soldStock.blade.php
+++ resources/views/soldStock.blade.php
@@ -102,7 +102,7 @@
             </div>
         </div>
         <br>
-        <div class="pageContainer" style="margin-left:222px;">
+        <div class="pageContainer" style="margin-left:160px;">
                  <div class="tab-content" id="pills-tabContent">
                     <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
                         aria-labelledby="pills-home-tab">
@@ -134,7 +134,7 @@
                                 
                                     </div>
                                 </div>
-                            </div> <br><br>
+                            </div>  </div><br><br>
         <div class="row">
             <div class="col-md-12">
                 <div class="receivingMain">
@@ -145,7 +145,7 @@
                             style="width: 100%; text-align: center;">
                             <thead>
                                 <tr>
-                                    <th></th>
+                                     
                                     <th>Product Id</th>
                                     <th>Company</th>
                                     <th>Product Name</th>
@@ -162,13 +162,13 @@
                             </tbody>
 
                         </table>
-                        <button onclick="hideColumn()">Hide</button>
+               
                         <button class="btn btn-success" onclick="printStockData()">Print </button>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+   
 </header>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -199,7 +199,7 @@
 
                     $.each(a, function (i, item) {
                         
-                        table.row.add([  a[i].ProductName,a[i].ProductID, a[i].Company, a[i]
+                        table.row.add([  a[i].ProductID, a[i].Company, a[i]
                             .ProductName, a[i].PerUnitSalePrice, a[i].PerUnitPurchasePrice, a[i]
                             .StockIn, a[i].EngineNumber, a[i].ChasisNumber,a[i].Status
                         ]);
@@ -229,7 +229,7 @@
         $('#stockTable').DataTable({
             responsive: {
                 details: {
-                    type: 'column',
+                    type: 'rows',
                     target: 'tr'
                 }
             },
@@ -338,36 +338,4 @@
 </body>
 
 </html>
-
-<!-- <script>
-        function getStock() {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-
-                if (this.readyState == 4 && this.status == 200) {
-
-                    var data = this.responseText;
-                    //alert(data);
-                    var table;
-                    var a = JSON.parse(data);
-                    //  alert(a[0].ProductSerial);
-                    table = $('#stockTable').DataTable();
-
-                    $.each(a, function (i, item) {
-
-                        table.row.add([a[i].ProductID,  a[i].Company, a[i].ProductName, a[i]
-                            .PerUnitSalePrice, a[i].PerUnitPurchasePrice, a[i].StockIn, a[i]
-                            .EngineNumber, a[i].ChasisNumber, a[i].Status
-                        ]);
-                    });
-                    table.columns.adjust().draw();
-
-                }
-            };
-            //alert("ljd");
-            xhttp.open("GET", "./viewStock/", true);
-
-            xhttp.send();
-        }
-
-    </script> -->
+ 
