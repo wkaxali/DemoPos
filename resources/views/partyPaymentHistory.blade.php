26fbf458ba44da0d6138ad76a84cddc4e43c2396
uzair <you@example.com>
2021-10-05 22:33:47 +0500

    ss

--- resources/views/partyPaymentHistory.blade.php
+++ resources/views/partyPaymentHistory.blade.php
@@ -610,9 +610,8 @@
             var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("supplier").innerHTML = 
-                    this.responseText;
-                 alert(this.responseText);
+                var a =this.responseText;
+                 alert(a);
 
             }
         };
